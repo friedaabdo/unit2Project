1d5433a3b84c11a87d2ef933c45e0a4e1c95d21a
Frieda Abdo <friedabdo@gmail.com>
2020-10-09 13:33:05 -0400

    im semi happy with it

--- src/Components/BJLP.js
+++ src/Components/BJLP.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './BJLP.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BJLP.css";
 // import {Route,Switch} from 'react-router-dom'
 
-
-
-
 const LandingPage = () => {
-    return(
-        <div className='landing'>
-            <h1>Let's Play BlackJack!</h1>
-            <Link to='/game'><button>Play!</button></Link>
-            <Link to='/rules'><button>Rules</button></Link>
+  return (
+    <div className="landing">
+      <h1>Let's Play BlackJack!</h1>
+      <p>*Junior Edition</p>
+      <Link to="/game">
+        <button>Play!</button>
+      </Link>
+      <Link to="/rules">
+        <button>Rules</button>
+      </Link>
+    </div>
+  );
+};
 
-         
-
-        </div>
-    )
-}
-
-export default LandingPage
+export default LandingPage;
